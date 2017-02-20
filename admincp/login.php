f8990bb7645fa2812f413307730d0c7378573920
manhnd <manh.it.it.it@gmail.com>
2017-02-20 22:49:54 +0700

    api login

--- admincp/login.php
+++ admincp/login.php
@@ -52,7 +52,7 @@ if (isset($_POST['txtUserName']) && isset($_POST['txtPassword'])) {
         $username = $_POST['txtUserName'];
         $password = $_POST['txtPassword'];
         $db = new DB_ADAPTER();
-        $con = array("username" => $username, "password" => $password);
+        $con = array("username" => $username, "password" => sha1($password));
         $result = $db->get_by_conditions('user', $con);
         if (count($result) > 0) {
             $_SESSION['token'] = sha1($username + $password);
---
+++ api/user/login.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manhi
+ * Date: 20/2/2017
+ * Time: 9:39 PM
+ */
+include '../../db/configdb.php';
+require_once('../../db/DB_ADAPTER.php');
+require_once('../Utils/Utils.php');
+$utils = new Utils();
+$response = array();
+if ($utils->checkHeader() == true) {
+    $username = "";
+    $password = "";
+    if (!isset($_POST['username'])) {
+        $username = "";
+    } else {
+        $username = $_POST['username'];
+    }
+    if (!isset($_POST['password'])) {
+        $password = "";
+    } else {
+        $password = $_POST['password'];
+    }
+    if (empty(trim($username))) {
+        $response['success'] = false;
+        $response['status'] = '401';
+        $response['message'] = 'Tên đăng nhập không được để trống';
+        echo json_encode($response, JSON_UNESCAPED_UNICODE);
+    } elseif (empty(trim($password))) {
+        $response['success'] = false;
+        $response['status'] = '401';
+        $response['message'] = 'Mật khẩu không được để trống';
+        echo json_encode($response, JSON_UNESCAPED_UNICODE);
+    } else {
+        $db = new DB_ADAPTER();
+        $con = array("username" => $username, "password" => sha1($password));
+        $result = $db->get_by_conditions('user', $con);
+        if (count($result) == 0 || $result[0]['type'] == 1) {
+            $response['success'] = false;
+            $response['status'] = '401';
+            $response['message'] = "Tên đăng nhập hoặc mật khẩu không đúng, vui lòng kiểm tra lại";
+            echo json_encode($response, JSON_UNESCAPED_UNICODE);
+        } else {
+            $token = sha1($username + time());
+            $object = array("token" => $token);
+            $condistion = array("id" => $result[0]['id']);
+            $result = $db->update("user", $object, $condistion);
+            if ($result == true) {
+                $response['success'] = true;
+                $response['status'] = '200';
+                $data['id'] = $result[0]['id'];
+                $data['username'] = $result[0]['username'];
+                $data['type'] = $result[0]['type'];
+                $data['token'] = $token;
+                $response['data'] = $data;
+                echo json_encode($response, JSON_UNESCAPED_UNICODE);
+            } else {
+                $response['success'] = false;
+                $response['status'] = '500';
+                $response['message'] = 'Có lỗi xảy ra, vui lòng thử lại sau';
+                echo json_encode($response, JSON_UNESCAPED_UNICODE);
+            }
+        }
+    }
+} else {
+    $response['success'] = false;
+    $response['status'] = '401';
+    $response['message'] = 'Missing api key';
+    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+}
+?>
