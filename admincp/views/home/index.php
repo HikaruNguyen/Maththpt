48abae5e9a0e09ad354705d4ad5732ebda57d588
HikaruNguyen <Manh.it.it.it@gmail.com>
2017-04-14 14:55:30 +0700

    thong ke usser

--- admincp/views/home/index.php
+++ admincp/views/home/index.php
@@ -87,6 +87,69 @@ if (isset($_SESSION['token'])) {
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <!-- BEGIN CHART PORTLET-->
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-bar-chart font-green-haze"></i>
+                        <span class="caption-subject bold uppercase font-green-haze"> Biểu đồ</span>
+                        <span class="caption-helper">Thống kê số lượng thành viên</span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div id="chart_4" class="chart" style="height: 400px;">
+                        <div id="charUser"></div>
+                        <?php
+                        //                        $sql_count = "SELECT id,displayname as name, countReview*100/(select sum(countReview) from " . CRUDUtils::$DB_BODE . " ) as countReview FROM " . CRUDUtils::$DB_BODE;
+                        $sql_count = "SELECT DATE_FORMAT(FROM_UNIXTIME(creat_at), '%d-%m-%Y') as 'Date', count('Date') as countDate FROM user group by DATE_FORMAT(FROM_UNIXTIME(creat_at), '%d-%m-%Y') ORDER BY creat_at asc";
+                        $result = $db->get_data_use_query($sql_count);
+
+                        $dataTests = array();
+                        if ($result != null && count($result) > 0) {
+                            for ($i = 0; $i < count($result); $i++) {
+                                array_push($dataTests, array("y" => $result[$i]["countDate"], "legendText" => $result[$i]["Date"], "label" => $result[$i]["Date"]));
+                            }
+                        }
+                        ?>
+                        <script type="text/javascript">
+                            $(function () {
+                                var chart = new CanvasJS.Chart("charUser", {
+//                                    title: {
+//                                        text: "Thống kê tỷ lệ ôn tập theo chuyên đề"
+//                                    },
+                                    animationEnabled: true,
+                                    legend: {
+                                        verticalAlign: "center",
+                                        horizontalAlign: "left",
+                                        fontSize: 15,
+                                        fontFamily: "Roboto"
+                                    },
+                                    theme: "theme1",
+                                    data: [
+                                        {
+                                            type: "line",
+                                            indexLabelFontFamily: "Garamond",
+                                            indexLabelFontSize: 13,
+                                            indexLabel: "{y}",
+                                            name: "Số lượng",
+                                            startAngle: -20,
+                                            showInLegend: true,
+                                            toolTipContent: "{legendText} {y}%",
+                                            dataPoints: <?php echo json_encode($dataTests, JSON_NUMERIC_CHECK); ?>
+                                        }
+                                    ]
+                                });
+                                chart.render();
+                            });
+                        </script>
+                    </div>
+                </div>
+            </div>
+            <!-- END CHART PORTLET-->
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-md-6">
@@ -166,7 +229,7 @@ if (isset($_SESSION['token'])) {
                         $dataPoints = array();
                         if ($result != null && count($result) > 0) {
                             for ($i = 0; $i < count($result); $i++) {
-                                array_push($dataPoints, array("countE" => $result[$i]["countE"],"y" => (float)round($result[$i]["countReview"], 2), "legendText" => $result[$i]["name"], "label" => $result[$i]["name"]));
+                                array_push($dataPoints, array("countE" => $result[$i]["countE"], "y" => (float)round($result[$i]["countReview"], 2), "legendText" => $result[$i]["name"], "label" => $result[$i]["name"]));
                             }
                         }
                         ?>
@@ -224,7 +287,7 @@ if (isset($_SESSION['token'])) {
                     <div id="chart_4" class="chart" style="height: 400px;">
                         <div id="chartTests"></div>
                         <?php
-//                        $sql_count = "SELECT id,displayname as name, countReview*100/(select sum(countReview) from " . CRUDUtils::$DB_BODE . " ) as countReview FROM " . CRUDUtils::$DB_BODE;
+                        //                        $sql_count = "SELECT id,displayname as name, countReview*100/(select sum(countReview) from " . CRUDUtils::$DB_BODE . " ) as countReview FROM " . CRUDUtils::$DB_BODE;
                         $sql_count = "SELECT id,displayname as name, countReview as countReview FROM " . CRUDUtils::$DB_BODE;
                         $result = $db->get_data_use_query($sql_count);
 
