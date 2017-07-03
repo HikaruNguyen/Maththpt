e82b5b0593c8280b8540318e7d86766877f363b6
manhnd <manhnd>
2017-07-03 21:01:54 +0700

    fix getcategory

--- api/content/get-category.php
+++ api/content/get-category.php
@@ -15,21 +15,9 @@ $response = array();
 const  per_page = 10;
 if ($utils->checkHeader() == true) {
     $db = new DB_ADAPTER();
-    $sql_get_count = "SELECT count(id) as SL from tbl_category";
-    $count_result = $db->get_data_use_query($sql_get_count);
-    $total_record = (int)$count_result[0]['SL'];
-    $total_page = $total_record / per_page;
-    if (!isset($_GET['page'])) {
-        $page = 0;
-    } else {
-        try {
-            $page = (int)$_GET['page'];
-        } catch (Exception $ex) {
-            $page = 0;
-        }
-    }
-    $begin_record = $page * per_page;
-    $result = $db->get_all_record_paging('tbl_category', $begin_record, per_page);
+    $sql_querry = "SELECT tbl_category.id, tbl_category.name, COUNT(tbl_category.id) as countQuestion FROM tbl_category, tbl_content WHERE tbl_category.id = tbl_content.cateID GROUP BY tbl_category.id, tbl_category.name";
+
+    $result = $db->get_data_use_query($sql_querry);
 
     if (http_response_code() == 200) {
         $response['success'] = true;
@@ -38,8 +26,6 @@ if ($utils->checkHeader() == true) {
         $response['success'] = false;
     }
     $response['status'] = http_response_code();
-    $response['totalPage'] = round($total_page, 0);
-    $response['currentPage'] = round($page, 0);
     $response['data'] = $result;
 
     echo json_encode($response, JSON_UNESCAPED_UNICODE);
